3872638fae06ca9c31f0a376e7fa8a01eb67332a
shun0301 <xxsu007xx@icloud.com>
2020-04-10 16:03:33 +0900

    トップページと一覧追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,6 @@
 class ToppagesController < ApplicationController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("created_at DESC").limit(9).order('created_at DESC')
   end
 
 end
